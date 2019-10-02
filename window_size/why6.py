4a6765c8dfffeabdb849e2e6467b13445e9a420e
Haoran <wang.8217@osu.edu>
2019-10-02 11:06:38 -0400

    compare benign and malicious

--- window_size/why6.py
+++ window_size/why6.py
@@ -2,16 +2,11 @@
 
 import sys
 import os
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix
-from multiprocessing import Pool
-import pickle as pkl
 import numpy as np
-import math 
 
-def build_feature_vector(win_list, train):
-    for filename in os.listdir(train):
-        with open(train + '/' + filename) as f:
+def build_feature_vector(win_list, benign):
+    for filename in os.listdir(benign):
+        with open(benign + '/' + filename) as f:
             first = f.readline()
             first = first[first.find(' ')+1: first.find('\n')]
             while first:
@@ -49,7 +44,7 @@ def construct_window_vector(file):
                 if window in windowCount.keys():
                     windowCount[window] += 1
                 else:
-                     windowCount["foreign"] = 1
+                     windowCount["foreign"] += 1
                 first = f.readline()
                 first = first[first.find(' ') + 1: first.find('\n')]
             # print(windowCount.values())
@@ -71,53 +66,47 @@ def construct_label_vector(file):
     return labels
 
 if __name__ == '__main__':
-    global apiWindowMap
-    global labelMap
+    global count
+    count = 0
 
     sys.stdout.write('Usage: python3 why6.py train/ test/ train_label.txt test_label.txt [window size]\n')
 
-    train = sys.argv[1]
-    test = sys.argv[2]
-    train_label_fn = sys.argv[3]
-    test_label_fn = sys.argv[4]
-    windowSize = int(sys.argv[5])
+    benign = sys.argv[1]
+    malicious = sys.argv[2]
+    #train_label_fn = sys.argv[3]
+    #test_label_fn = sys.argv[4]
+    windowSize = int(sys.argv[3])
 
     # Build feature vectors for api files
     sys.stdout.write('Reading in api.txt files...\n')
     win_list = list()
-    build_feature_vector(win_list, train)
+    build_feature_vector(win_list, benign)
     win_list.append("foreign")
     sys.stdout.write('Done\n')
 
     # Construct vector for each training files
     sys.stdout.write('Constructing window vector for train and test files...\n')
-    trainApiWindow = construct_window_vector(train)
-    x_train = np.asarray(list(trainApiWindow.values()))
-    testApiWindow = construct_window_vector(test)
+    #trainApiWindow = construct_window_vector(benign)
+    #x_train = np.asarray(list(trainApiWindow.values()))
+    testApiWindow = construct_window_vector(malicious)
     x_test = np.asarray(list(testApiWindow.values()))
 
     sys.stdout.write("Done\n")
-    #print(list(trainApiWindow.values()))
+
+    for k,v in testApiWindow:
+        print(k, v["foreign"])
 
     # Construct label vector for training data
-    train_label = construct_label_vector(train_label_fn)
-    y_train = np.asarray(train_label)
+    #train_label = construct_label_vector(train_label_fn)
+    #y_train = np.asarray(train_label)
 
     # Construct label vector for testing data
-    test_label = construct_label_vector(test_label_fn)
-    y_test = np.asarray(test_label)
+    #test_label = construct_label_vector(test_label_fn)
+    #y_test = np.asarray(test_label)
 
     # print(x_train.shape)
     # print(y_train.shape)
     # print(x_test.shape)
     # print(y_test.shape)
     # fit svm with training data and label
-    svclassifier = SVC(kernel='poly', gamma="auto")
-    svclassifier.fit(x_train, y_train)
-
-    # predict testing label
-    y_pred = svclassifier.predict(x_test)
-
-    # print confusion matrix
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+   
